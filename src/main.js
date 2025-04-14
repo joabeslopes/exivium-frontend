a7b8c202b5fe541296ce67101716c58906983027
joabeslopes <joabeslopes.jsl@gmail.com>
2025-04-14 15:41:39 -0300

    mudança da tag img para a tag canvas

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 const token = "meu_token";
 const ws = new WebSocket(`ws://localhost:8000/ws/video?token=${token}`);
-const img = document.getElementById("video");
+const canvas = document.getElementById("video");
+const ctx = canvas.getContext("2d");
 
 ws.binaryType = "blob";
-ws.onmessage = (event) => {
-  const url = URL.createObjectURL(event.data);
-  img.src = url;
+ws.onmessage = async (event) => {
+  try {
+    const bitmap = await createImageBitmap(event.data);
+    ctx.drawImage(bitmap, 0, 0, canvas.width, canvas.height);
+  } catch (error) {
+    console.error("Erro ao desenhar imagem:", error);
+  }
 };
 
 ws.onerror = (event) => {
